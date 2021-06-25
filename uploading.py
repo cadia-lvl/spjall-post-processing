44f1a4b0a37b6eb1b6d6bd7125f1a218dc00ca42
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-06-25 12:27:58 +0000

    added docstrings and fixed empty lines

--- uploading.py
+++ uploading.py
@@ -11,18 +11,19 @@ tokens = json.load(token_file)
 extracted_file = open('transcripts.json')
 extracted = json.load(extracted_file)
 
-
 API_TOKEN = tokens['API_TOKEN']
-
 
 response = requests.get(urls['spjall_url'])
 
 samromur_url = urls['samromur_url']
 
+
 def post(extracted,response):
+  '''checks if the audio file is already on tiro. Creates body for the files and posts them to tiro.'''
   for elem in extracted:
     if elem['metadata']['subject'][0:36] == response.json()[1]['session_id']:
       return None
+
   test_a = samromur_url + "/" + response.json()[1]['session_id']+"/"+response.json()[1]['client_a']['session_id']+'/_client_a.wav'
   test_b = samromur_url + "/" + response.json()[1]['session_id']+"/"+response.json()[1]['client_b']['session_id']+'/_client_b.wav'
   subject_a = response.json()[1]['session_id']+'_client_a.wav'
@@ -30,26 +31,20 @@ def post(extracted,response):
 
   body_a = create_body(subject_a,test_a)
 
-
   authorization = {'Authorization' : "Bearer {}".format(API_TOKEN)}
   a_res = requests.post(urls['tiro_url'],data=json.dumps(body_a),headers=authorization)
   print(a_res)
-
   body_b = create_body(subject_b,test_b)
-
-
-  authorization = {'Authorization' : "Bearer {}".format(API_TOKEN)}
-
-
   b_res =requests.post(urls['tiro_url'],data=json.dumps(body_b),headers=authorization)
   print(b_res)
 
   return None
 
-
 post(extracted,response)
 
+
 def create_body(subject,test):
+  '''helperfunction that creates the body for the files'''
   body = {
   "metadata": {
     "fileType": "AUDIO",
@@ -69,7 +64,6 @@ def create_body(subject,test):
   
   }
   return body
-
 
 print('test')
 # /usr/local/bin/python3 /Users/malla/Documents/spjall-post-processing/extract.py /Users/malla/Documents/spjall-post-processing/urls.json /Users/malla/Documents/spjall-post-processing/token.json
