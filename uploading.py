782965fbe3c1118953b1d48018d1d7d279d27a84
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-07-05 08:52:19 +0000

    fixed most of the comments

--- uploading.py
+++ uploading.py
@@ -1,74 +1,93 @@
+# Author: Málfríður Anna Eiríksdóttir
+
 import json
 import requests
 from requests.api import head
-
-urls_file = open('urls.json')
-urls = json.load(urls_file)
-
-token_file = open('token.json')
-tokens = json.load(token_file)
-
-extracted_file = open('transcripts.json')
-extracted = json.load(extracted_file)
-
-API_TOKEN = tokens['API_TOKEN']
-
-response = requests.get(urls['spjall_url'])
-
-samromur_url = urls['samromur_url']
+from extract import Extraction
 
 
-def post(extracted,response):
-  '''checks if the audio file is already on tiro. Creates body for the files and posts them to tiro.'''
-  is_a = False
-  is_b = False
-  for elem in extracted:
-    if elem['metadata']['subject'][0:36] == response.json()[1]['session_id']:
-      if elem['metadata']['subject'][44] == "a":
-        is_a = True
-      elif elem['metadata']['subject'][44] == "b":
-        is_b = True
-    if is_a and is_b:
-      return None
+def post(extracted,spjall_response):
+    '''checks if the audio file is already on tiro. Creates body for the files and posts them to tiro.'''
+    print('Posting files to tiro')
+    counter = 0
+    for i in range(len(extracted)):
+      is_a = False
+      is_b = False
+      for elem in extracted:
+        if spjall_response.json()[i]['session_id'] in elem['metadata']['subject']:
+          if 'client_a' in elem['metadata']['subject']:
+            is_a = True
+          elif 'client_b' in elem['metadata']['subject']:
+            is_b = True
+        if is_a and is_b:
+          return None
 
-  authorization = {'Authorization' : "Bearer {}".format(API_TOKEN)}
+      authorization = {'Authorization': "Bearer {}".format(API_TOKEN)}
 
-  if not is_a:
-    test_a = samromur_url + "/" + response.json()[1]['session_id']+"/"+response.json()[1]['client_a']['session_id']+'/_client_a.wav'
-    subject_a = response.json()[1]['session_id']+'_client_a.wav'
-    body_a = create_body(subject_a,test_a)
-    a_res = requests.post(urls['tiro_url'],data=json.dumps(body_a),headers=authorization)
-    print(a_res)
+      if not is_a:
+        test_a = samromur_url + "/" + spjall_response.json()[i]['session_id']+"/"+spjall_response.json()[i]['session_id']+'/_client_a.wav'
+        subject_a = spjall_response.json()[i]['session_id']+'_client_a.wav'
+        body_a = create_body(subject_a,test_a)
+        a_res = requests.post(urls['tiro_url'],data=json.dumps(body_a),headers=authorization)
+        counter += 1
+        print(a_res) #should print to a file instead
+        if counter>=10:
+          return
+        
 
-  if not is_b:
-    test_b = samromur_url + "/" + response.json()[1]['session_id']+"/"+response.json()[1]['client_b']['session_id']+'/_client_b.wav'
-    subject_b = response.json()[1]['session_id']+'_client_b.wav'
-    body_b = create_body(subject_b,test_b)
-    b_res =requests.post(urls['tiro_url'],data=json.dumps(body_b),headers=authorization)
-    print(b_res)
+      if not is_b:
+        test_b = samromur_url + "/" + spjall_response.json()[i]['session_id']+"/"+spjall_response.json()[i]['session_id']+'/_client_b.wav'
+        subject_b = spjall_response.json()[i]['session_id']+'_client_b.wav'
+        body_b = create_body(subject_b,test_b)
+        b_res = requests.post(urls['tiro_url'],data=json.dumps(body_b),headers=authorization)
+        counter += 1
+        print(b_res) #should print to a file instead
+        if counter>=10:
+          return
+        
 
-  return None
+    return None
 
-post(extracted,response)
 
 
 def create_body(subject,test):
-  '''helperfunction that creates the body for the files'''
-  body = {
-  "metadata": {
-    "fileType": "AUDIO",
-    "languageCode": "is-IS",
-    "subject": subject,
-    "description": "",
-    "keywords": [
-      "this is a test",
-      #"__spjallromur__",
-      #"M\u00e1lt\u00e6kni\u00e1\u00e6tlun",
-      #"TODO"
-    ]
-  },
-  "useUri": True,
-  "uri": test
-  
-  }
-  return body
+    '''helper function that creates the body for the tiro/talgreinir submit post request'''
+    body = {
+    "metadata": {
+      "fileType": "AUDIO",
+      "languageCode": "is-IS",
+      "subject": subject,
+      "description": "",
+      "keywords": [
+        "this is a test",
+        #"__spjallromur__",
+        #"M\u00e1lt\u00e6kni\u00e1\u00e6tlun",
+        #"TODO"
+      ]
+    },
+    "useUri": True,
+    "uri": test
+    
+    }
+    return body
+
+if __name__ == "__main__":
+    urls_file = open('urls.json')
+    urls = json.load(urls_file)
+
+    token_file = open('token.json')
+    tokens = json.load(token_file)
+
+    extracted_file = open('transcripts.json')
+    extracted = json.load(extracted_file)
+
+    API_TOKEN = tokens['API_TOKEN']
+
+    spjall_response = requests.get(urls['spjall_url'])
+
+    samromur_url = urls['samromur_url']
+
+    transcripts_objects = Extraction(urls,tokens)
+    extracted = transcripts_objects.filter_transcripts('__spjallromur__')
+
+    #post(extracted,spjall_response)
